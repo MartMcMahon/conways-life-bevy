2d993e3d95309c1415cf0e9fcd9cbb90a66ebce3
Mart <themcman@gmail.com>
2021-06-27 13:30:41 -0500

    scrolling pattern

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,11 @@ fn main() {
         })
         .add_startup_system(setup.system())
         .add_startup_stage("grid", SystemStage::single(spawn_grid.system()))
-        // .add_system_set(
-        //     SystemSet::new()
-        //         .with_run_criteria(FixedTimestep::step(1.0))
-        //         .with_system(iteration.system()),
-        // )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(iteration.system()),
+        )
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -76,38 +76,16 @@ struct Materials {
     black_material: Handle<ColorMaterial>,
     grey_material: Handle<ColorMaterial>,
 }
+#[derive(Copy, Clone)]
 enum Cellstate {
     Alive,
     Dead,
 }
+#[derive(Copy, Clone)]
 struct Cell {
     x: f32,
     y: f32,
     state: Cellstate,
-    // neighbors are index of cell
-    // north: Option<*const Cell>,
-    // east: Option<*const Cell>,
-    // south: Option<*const Cell>,
-    // west: Option<*const Cell>,
-}
-impl Cell {
-    fn count_live_neighbors(&self) -> i32 {
-        let count = 0;
-        // n
-
-        // s
-        // e
-        // w
-        // return total
-        count
-    }
-
-    fn iterate(&mut self) -> Cellstate {
-        match self.count_live_neighbors() {
-            3 => Cellstate::Alive,
-            _ => Cellstate::Dead,
-        }
-    }
 }
 
 fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
@@ -166,7 +144,6 @@ fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
     ];
 
     let mut rng = rand::thread_rng();
-    let b: bool = rng.gen();
 
     for y in 0..10 {
         let y = y as f32;
@@ -175,6 +152,7 @@ fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
             if x < 3.0 && y < 3.0 {
                 cells.push(temp.remove(0));
             } else {
+                let b: bool = rng.gen();
                 cells.push(Cell {
                     x: x as f32,
                     y: y as f32,
@@ -192,7 +170,6 @@ fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
     //         cells[y * WIDTH + x].north = *cells[(y - 1) * WIDTH + x];
     //     }
     // }
-
     // let mut neighbor_refs: Vec<&Cell> = Vec::new();
 
     for y in 0..10 {
@@ -201,11 +178,16 @@ fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
             let x = x as f32;
             let index = y * WIDTH as f32 + x;
 
+            let mat = match &cells[0].state {
+                Cellstate::Alive => materials.white_material.clone(),
+                Cellstate::Dead => materials.grey_material.clone(),
+            };
+
             commands
                 .spawn()
                 .insert(cells.remove(0))
                 .insert_bundle(SpriteBundle {
-                    material: materials.square.clone(),
+                    material: mat,
                     sprite: Sprite::new(Vec2::new(SQUARE_SIZE, SQUARE_SIZE)),
                     ..Default::default()
                 })
@@ -218,60 +200,98 @@ fn spawn_grid(mut commands: Commands, materials: Res<Materials>) {
     }
 }
 
+fn get_neighbors(world: &Vec<Cell>, index: usize) -> Vec<Cell> {
+    world.clone()
+}
+
 fn iteration(
     mut commands: Commands,
     mut cells: Query<(&mut Cell, &mut Handle<ColorMaterial>)>,
     materials: Res<Materials>,
 ) {
-    for (mut cell, mut mat) in cells.iter_mut() {
-        // check neighbors
-        // cells.iter_mut().map(|(mut neighbor_cell, _)| {
-        //     match neighbor_cell.y {
-        //         cell.y + 1.0 => {
-        //             // north neighbor
-        //         },
-        //         cell.y - 1.0 => {
-        //             // sourth
-        //         }
-        //     };
-        //     match neighbor_cell.x {
-        //         cell.x -1.0 => {
-        //             // west
-        //         },
-        //         cell.x +1.0 => {
-        //             // east
-        //         }
-        //     }
-        // });
+    let mut old_world: Vec<Cell> = Vec::new();
+    let mut c: i32 = 0;
 
-        match &cell.count_live_neighbors() {
-            3 => {
+    for (mut cell, mut mat) in cells.iter_mut() {
+        old_world.push(cell.clone());
+    }
+    let mut new_world = old_world.clone();
+
+    for (i, cell) in old_world.iter().enumerate() {
+        get_neighbors(&old_world, i);
+        if i > 0 {
+            new_world[i - 1] = cell.clone();
+        }
+    }
+
+    for (i, (mut cell, mut mat)) in cells.iter_mut().enumerate() {
+        match new_world[i].state {
+            Cellstate::Alive => {
                 cell.state = Cellstate::Alive;
                 *mat = materials.white_material.clone();
             }
-            _ => {
+            Cellstate::Dead => {
                 cell.state = Cellstate::Dead;
                 *mat = materials.grey_material.clone();
             }
         }
-        // match &cell.state {
-        // Cellstate::Alive => {
-        //     match cell.count_live_neighbors(&cell) {
-        // _ => {}
-        // }
-
-        // fewer than two live neighbors, die
-        // 2-3 live neighbors, live on
-        // more than 3, die
-        //
-        // cell.state = Cellstate::Dead;
-        // }
-        // Cellstate::Dead => {
-        // // ==3 live neighbors, become alive
-        // // cell.state = Cellstate::Alive;
-        // }
-        // }
     }
+
+    // if c % 2 == 0 {
+    //     cell.state = Cellstate::Alive;
+    //     *mat = materials.white_material.clone();
+    // } else {
+    //     cell.state = Cellstate::Dead;
+    //     *mat = materials.grey_material.clone();
+    // }
+    // c += 1;
+    // check neighbors
+    // cells.iter_mut().map(|(mut neighbor_cell, _)| {
+    //     match neighbor_cell.y {
+    //         cell.y + 1.0 => {
+    //             // north neighbor
+    //         },
+    //         cell.y - 1.0 => {
+    //             // sourth
+    //         }
+    //     };
+    //     match neighbor_cell.x {
+    //         cell.x -1.0 => {
+    //             // west
+    //         },
+    //         cell.x +1.0 => {
+    //             // east
+    //         }
+    //     }
+    // });
+
+    // match &cell.count_live_neighbors() {
+    //     3 => {
+    //         cell.state = Cellstate::Alive;
+    //         // *mat = materials.white_material.clone();
+    //     }
+    //     _ => {
+    //         cell.state = Cellstate::Dead;
+    //         // *mat = materials.grey_material.clone();
+    //     }
+    // }
+    // match &cell.state {
+    // Cellstate::Alive => {
+    //     match cell.count_live_neighbors(&cell) {
+    // _ => {}
+    // }
+
+    // fewer than two live neighbors, die
+    // 2-3 live neighbors, live on
+    // more than 3, die
+    //
+    // cell.state = Cellstate::Dead;
+    // }
+    // Cellstate::Dead => {
+    // // ==3 live neighbors, become alive
+    // // cell.state = Cellstate::Alive;
+    // }
+    // }
 }
 
 fn square_spawner(mut commands: Commands, materials: Res<Materials>) {
