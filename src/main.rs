9d682aa9d9fab5fe76abb6beddbcafa030bd2bc5
Mart <themcman@gmail.com>
2021-08-03 01:50:41 -0400

    stable and compiles

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ fn main() {
         .init_resource::<ButtonMaterials>()
         .add_startup_system(setup.system())
         .add_startup_stage("grid", SystemStage::single(spawn_grid.system()))
-        .add_system(button_system.system())
+        // .add_system(button_system.system())
         .add_system(input_system.system())
         .run();
 }
@@ -103,32 +103,32 @@ impl FromWorld for ButtonMaterials {
     }
 }
 
-fn button_system(
-    button_materials: Res<ButtonMaterials>,
-    mut interaction_query: Query<
-        (&Interaction, &mut Handle<ColorMaterial>, &Children),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut material, children) in interaction_query.iter_mut() {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                *material = button_materials.pressed.clone();
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *material = button_materials.hovered.clone();
-            }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *material = button_materials.normal.clone();
-            }
-        }
-    }
-}
+// fn button_system(
+//     button_materials: Res<ButtonMaterials>,
+//     mut interaction_query: Query<
+//         (&Interaction, &mut Handle<ColorMaterial>, &Children),
+//         (Changed<Interaction>, With<Button>),
+//     >,
+//     mut text_query: Query<&mut Text>,
+// ) {
+//     for (interaction, mut material, children) in interaction_query.iter_mut() {
+//         let mut text = text_query.get_mut(children[0]).unwrap();
+//         match *interaction {
+//             Interaction::Clicked => {
+//                 text.sections[0].value = "Press".to_string();
+//                 *material = button_materials.pressed.clone();
+//             }
+//             Interaction::Hovered => {
+//                 text.sections[0].value = "Hover".to_string();
+//                 *material = button_materials.hovered.clone();
+//             }
+//             Interaction::None => {
+//                 text.sections[0].value = "Button".to_string();
+//                 *material = button_materials.normal.clone();
+//             }
+//         }
+//     }
+// }
 
 fn setup(
     mut commands: Commands,
@@ -138,32 +138,32 @@ fn setup(
 ) {
     // ui camera
     commands.spawn_bundle(UiCameraBundle::default());
-    commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(65.0), Val::Px(65.0)),
-                margin: Rect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..Default::default()
-            },
-            material: button_materials.normal.clone(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Button",
-                    TextStyle {
-                        font: asset_server.load("fonts/helvetica.ttf"),
-                        font_size: 30.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                    Default::default(),
-                ),
-                ..Default::default()
-            });
-        });
+    // commands
+    //     .spawn_bundle(ButtonBundle {
+    //         style: Style {
+    //             size: Size::new(Val::Px(65.0), Val::Px(65.0)),
+    //             margin: Rect::all(Val::Auto),
+    //             justify_content: JustifyContent::FlexStart,
+    //             align_items: AlignItems::Center,
+    //             ..Default::default()
+    //         },
+    //         material: button_materials.normal.clone(),
+    //         ..Default::default()
+    //     })
+    //     .with_children(|parent| {
+    //         parent.spawn_bundle(TextBundle {
+    //             text: Text::with_section(
+    //                 "Button",
+    //                 TextStyle {
+    //                     font: asset_server.load("fonts/helvetica.ttf"),
+    //                     font_size: 30.0,
+    //                     color: Color::rgb(0.9, 0.9, 0.9),
+    //                 },
+    //                 Default::default(),
+    //             ),
+    //             ..Default::default()
+    //         });
+    //     });
 
     let white_mat = materials.add(Color::rgb(0.9, 0.9, 0.9).into());
     let black_mat = materials.add(Color::rgb(0.1, 0.1, 0.1).into());
